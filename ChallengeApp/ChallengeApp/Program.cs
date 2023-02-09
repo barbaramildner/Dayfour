e87bada991a01feddc52aca4d50459eb74edb842
Barbara M <barbara.mildner@gmail.com>
2023-02-09 15:28:53 +0100

    Zadanie, dzień 4

---
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -0,0 +1,24 @@
+﻿var name = "Anna";
+bool woman = true;
+int age = 40;
+
+if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (woman == false && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else if (woman == true && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else
+{
+    Console.WriteLine("40 letnia kobieta o imieniu Anna");
+}
+// Ewa, lat 30
+// Niepełnoletni mężczyzna
+// Kobieta poniżej 30 lat
+// 40 letnia kobieta o imieniu Anna
